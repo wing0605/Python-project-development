86be92466c798f6259744a8d0ddc219d5aa534d6
wing0605 <wing0605@users.noreply.github.com>
2021-01-13 21:33:50 +0800

    玛丽冒险

---
+++ marie_adventure/marie.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+'''==============================================
+@Project -> File   ：Python项目开发实战入门 -> marie
+@IDE    ：PyCharm
+@Author ：Liu Yimin
+@Date   ：2021-01-13 21:32
+@Desc   ：
+==============================================='''
